a8e59754594b73f64b216c7bfe977c466532e7f4
yigitkg <yigitkg@gmail.com>
2023-05-29 19:42:06 +0300

    API integration started

--- src/routes/Discover/components/Discover.tsx
+++ src/routes/Discover/components/Discover.tsx
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
 import "../styles/_discover.scss";
 
-//TODO: Fix `any` types here
-
 interface IDiscoverProps {}
 
 interface IDiscoverState {
@@ -41,7 +39,80 @@ export default class Discover extends Component<
     };
   }
 
-  //TODO: Handle APIs
+  componentDidMount() {
+    this.fetchNewReleases();
+    this.fetchFeaturedPlaylists();
+    this.fetchCategories();
+  }
+
+  fetchNewReleases = async () => {
+    try {
+      const response = await fetch(
+        "https://api.spotify.com/v1/browse/new-releases",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_SPOTIFY_ACCESS_TOKEN}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error fetching new releases");
+      }
+
+      const data = await response.json();
+
+      this.setState({ newReleases: data.albums.items });
+    } catch (error) {
+      console.error("Error fetching new releases:", error);
+    }
+  };
+
+  fetchFeaturedPlaylists = async () => {
+    try {
+      const response = await fetch(
+        "https://api.spotify.com/v1/browse/featured-playlists",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_SPOTIFY_ACCESS_TOKEN}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error fetching featured playlists");
+      }
+
+      const data = await response.json();
+
+      this.setState({ playlists: data.playlists.items });
+    } catch (error) {
+      console.error("Error fetching featured playlists:", error);
+    }
+  };
+
+  fetchCategories = async () => {
+    try {
+      const response = await fetch(
+        "https://api.spotify.com/v1/browse/categories",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_SPOTIFY_ACCESS_TOKEN}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error fetching categories");
+      }
+
+      const data = await response.json();
+
+      this.setState({ categories: data.categories.items });
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
 
   render() {
     const { newReleases, playlists, categories } = this.state;
